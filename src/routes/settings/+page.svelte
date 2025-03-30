52635704baefa06c2d4ef9a82907d78c089f2a0f
Lorre Huggan <lorre.huggan@protonmail.com>
2025-03-30 13:26:56 +0100

    wip: add settings page

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Switch from "@/components/ui/switch/switch.svelte";
+
+  const html = document.querySelector("html");
+  let switchToggle = $state(false);
+
+  $effect(() => {
+    if (switchToggle) {
+      html?.classList.add("dark");
+    } else {
+      html?.classList.remove("dark");
+    }
+  });
+
+  // function toggleTheme() {
+  //   if (!html) return;
+  //   if (html.classList.contains("dark")) {
+  //     html.classList.remove("dark");
+  //     switchToggle = false;
+  //   } else {
+  //     html.classList.add("dark");
+  //     switchToggle = true;
+  //   }
+  // }
+</script>
+
+<p>Settings</p>
+<Switch bind:checked={switchToggle} />
