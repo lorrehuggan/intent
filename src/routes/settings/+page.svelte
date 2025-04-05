7c53d72902369a2ade19bef59075aeec8af8f1a5
Lorre Huggan <lorre.huggan@protonmail.com>
2025-04-05 13:19:17 +0100

    wip: settings page

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,28 +1,48 @@
 <script>
   import Switch from "@/components/ui/switch/switch.svelte";
+  import { getUserSettings } from "@/context/user/settings.svelte";
+  import { invoke } from "@tauri-apps/api/core";
+  import { onMount } from "svelte";
 
   const html = document.querySelector("html");
+  const userSettings = getUserSettings();
   let switchToggle = $state(false);
 
-  $effect(() => {
-    if (switchToggle) {
-      html?.classList.add("dark");
+  onMount(() => {
+    if (!html) return;
+    if (html.classList.contains("dark")) {
+      switchToggle = true;
     } else {
-      html?.classList.remove("dark");
+      switchToggle = false;
     }
   });
 
-  // function toggleTheme() {
-  //   if (!html) return;
-  //   if (html.classList.contains("dark")) {
-  //     html.classList.remove("dark");
-  //     switchToggle = false;
-  //   } else {
-  //     html.classList.add("dark");
-  //     switchToggle = true;
-  //   }
-  // }
+  async function toggleTheme() {
+    if (!userSettings || !html) return;
+    const classList = html.classList;
+    if (classList.contains("dark")) {
+      classList.remove("dark");
+      switchToggle = false;
+      try {
+        await invoke("set_user_settings", { payload: { ...userSettings, theme: "light" } });
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      classList.add("dark");
+      switchToggle = true;
+      try {
+        await invoke("set_user_settings", { payload: { ...userSettings, theme: "dark" } });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
 </script>
 
-<p>Settings</p>
-<Switch bind:checked={switchToggle} />
+<h1 class="text-3xl">Settings</h1>
+<a href="/">home</a>
+<div class="flex-center gap-4">
+  <p>{`Set theme to ${switchToggle ? "light" : "dark"} mode`}</p>
+  <Switch onCheckedChange={toggleTheme} bind:checked={switchToggle} />
+</div>
